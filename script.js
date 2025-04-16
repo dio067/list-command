d44ba6c15f4084e3c573a9d049fa7fd7e2209952
dio067 <yousefnas06@gmail.com>
2025-04-16 14:11:53 +0400

    feat: installing chalk

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
+const chalk = require('chalk');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
         console.log(err);
     } 
 
-    console.log(filenames);
+    
+	for (let filename of filenames) {
+		try {
+			const stat = await fs.promises.lstat(filename);
+
+                if(stat.isFile()) {
+                    console.log(filename,' ');
+                } else {
+                    console.log(chalk.bold(chalk.blue(filename)),' ');
+                }
+
+		} catch (err) {
+			console.log(err);
+		}
+	}
 })
